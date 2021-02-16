18454d97f020cc7a43487417625329d3ee5cc7ee
saumyajaiswal2805 <78247155+saumyajaiswal2805@users.noreply.github.com>
2021-02-17 00:08:39 +0530

    Update assignment2.1.py

--- assignment2.1.py
+++ assignment2.1.py
@@ -10,3 +10,13 @@ while tries < 1:
     if coin == 2:
         print ('Tails')
 total = tries
+
+C:\Users\user\PycharmProjects\pythonProject\venv\Scripts\python.exe C:/Users/user/PycharmProjects/pythonProject/assignment2.1.py
+{'Tails', 'Heads'}
+Heads
+
+C:\Users\user\PycharmProjects\pythonProject\venv\Scripts\python.exe C:/Users/user/PycharmProjects/pythonProject/assignment2.1.py
+{'Heads', 'Tails'}
+Heads
+
+Process finished with exit code 0
